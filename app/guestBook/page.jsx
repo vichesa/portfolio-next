9f6c86a55d186b679d459f0d5327918028062aa6
vichesa <vichesay@gmail.com>
2023-10-22 23:15:40 +0700

    local endpoint

--- app/guestBook/page.jsx
+++ app/guestBook/page.jsx
@@ -1,30 +1,41 @@
-// Import required dependencies using ESM syntax
-import fetch from "node-fetch";
+import AddPost from "../components/AddPost";
+import PostList from "../components/PostList";
 
-export async function getGuestBookData() {
-  try {
-    // Use relative URL to the API route
-    const res = await fetch("/api/posts", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+async function getData() {
+  const res = await fetch("http://localhost:3001/api/posts", {
+    cache: "no-store",
+  });
 
-    if (!res.ok) {
-      // Handle non-successful HTTP responses
-      throw new Error(`Failed to fetch data: ${res.status} ${res.statusText}`);
-    }
-
-    const data = await res.json();
-
-    // Sort data if needed
-    data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-
-    return data;
-  } catch (error) {
-    // Handle errors and provide a more specific error message
-    console.error("Error fetching data:", error);
-    throw new Error("Failed to fetch data. Please try again later.");
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
   }
+
+  return res.json();
 }
+
+const Crud = async () => {
+  const posts = await getData();
+  posts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
+  return (
+    <section>
+      {/* <div className='my-5 flex flex-col gap-4'> */}
+      {/* <h1 className='text-3xl font-bold'>Todo List App</h1> */}
+      <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-10">
+        <div>
+          <h1 className="text-4xl font-bold text-center lg:text-left">
+            Guest Book
+          </h1>
+          <div className="my-10">
+            <AddPost />
+          </div>
+        </div>
+        <div className="lg:ml-10 mt-6">
+          <PostList posts={posts} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Crud;
